83c7ace1869b24ad5a9982b53ce1ce5f1bdb69e4
Teddilyn Gavozzi <tgavozzi@kent.edu>
2017-09-21 15:23:35 -0400

    change class

---
+++ Capstone/Classes/Change.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Classes;
+
+namespace Capstone.Classes
+{
+    public class Change
+    {
+        private int dimes;
+        private int nickels;
+        private int quarters;
+        private double totalChange;
+
+
+        public int Dimes
+        {
+            get { return dimes; }
+        }
+
+        public int Nickels
+        {
+            get { return nickels; }
+        }
+
+        public int Quarters
+        {
+            get { return quarters; }
+        }
+
+        public double TotalChange
+        {
+            get { return (dimes * 10 + nickels * 5 + quarters * 25); }
+        }
+
+        public Change(decimal amountInDollars)
+        {
+            amountInDollars = amountInDollars * 100;
+            while (amountInDollars >= 25)
+            {
+                this.quarters += 1;
+                amountInDollars -= 25;
+            }
+            while (amountInDollars >= 10)
+            {
+                this.dimes += 1;
+                amountInDollars -= 10;
+            }
+            while (amountInDollars >= 5)
+            {
+                this.nickels += 1;
+                amountInDollars -= 5;
+            }
+
+        }
+        public Change(int amountInCents)
+        {
+            while (amountInCents >= 25)
+            {
+                this.quarters += 1;
+                amountInCents -= 25;
+            }
+            while (amountInCents >= 10)
+            {
+                this.dimes += 1;
+                amountInCents -= 10;
+            }
+            while (amountInCents >= 5)
+            {
+                this.nickels += 1;
+                amountInCents -= 5;
+            }
+
+
+
+        }
+    }
+}
+
+
---
+++ CapstoneTests/Change35CentTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class Change35CentTests
+    {
+        [TestMethod]
+        public void Will35CentRenturn1QAnd1Dime()
+        {
+            Change thirtyFive = new Change(35);
+
+            Assert.AreEqual(1, thirtyFive.Quarters);
+            Assert.AreEqual(1, thirtyFive.Dimes);
+        }
+
+        [TestMethod]
+        public void WillOneDollar65CentsCentRenturn1QAnd1Dime()
+        {
+            Change one65 = new Change(1.65M);
+
+            Assert.AreEqual(6, one65.Quarters);
+            Assert.AreEqual(1, one65.Dimes);
+            Assert.AreEqual(1, one65.Nickels);
+        }
+    }
+}
